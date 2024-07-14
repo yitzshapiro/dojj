f5a5ba111495ce9144e382f7ba45c3ab901453c4
Yitzchak Shapiro <yitzashapiro@gmail.com>
2024-07-14 18:47:00 -0400

    Inital Commit

--- node_modules/.pnpm/@mlc-ai+web-llm@0.2.48/node_modules/@mlc-ai/web-llm/lib/openai_api_protocols/apis.d.ts
+++
@@ -1,8 +0,0 @@
-import { MLCEngineInterface } from "../types";
-import { Completions } from "./chat_completion";
-export declare class Chat {
-    private engine;
-    completions: Completions;
-    constructor(engine: MLCEngineInterface);
-}
-//# sourceMappingURL=apis.d.ts.map
